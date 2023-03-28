b77da339fff62e29b3d79bd9b97420a9ba12059c
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-28 17:22:37 -0600

    Unificación de cuentas a doc cuenta

---
+++ Cuenta.js
@@ -0,0 +1,62 @@
+export class Cuenta {
+    #cliente;
+    #saldo;
+
+    constructor(cliente, numero, agencia, saldo){
+       this.numero = numero;
+       this.agencia = agencia; 
+       this.#cliente = cliente;
+       this.#saldo = saldo;
+    }
+    #cliente;
+    numero;
+    agencia;
+    #saldo;
+    static cantidadCuentas = 0;
+
+    set cliente(valor) {
+        if (valor instanceof Cliente)
+            this.#cliente = valor;
+    }
+
+    get cliente() {
+        return this.#cliente;
+    }
+
+    constructor(cliente, numero, agencia) {
+        this.cliente = cliente;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.#saldo = 0;
+        CuentaCorriente.cantidadCuentas++;
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+        valor = 200;
+        valor = valor*1000;
+    }
+
+
+
+
+
+
+}
